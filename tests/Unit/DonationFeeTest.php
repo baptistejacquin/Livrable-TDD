7e382a2792169d6c8c36e41979efa4e52a9f7f0d
Baptiste Jacquin <baptiste.jacquin@Digital-Grenoble.local>
2018-05-14 14:49:00 +0200

    Test pour le calcul des commitions avec le concept GIVEN-WHEN-THEN

--- tests/Unit/DonationFeeTest.php
+++ tests/Unit/DonationFeeTest.php
@@ -16,13 +16,13 @@ class DonationFeeTest extends TestCase
      */
     public function testCommissionAmountGetter()
     {
-        // Etant donné une donation de 100 et commission de 10%
+        // Etant donné une donation de 3700 et commission de 30%
         $donationFees = new DonationFee(3700, 30);
 
         // Lorsque qu'on appel la méthode getCommissionAmount()
         $actual = $donationFees->getCommissionAmount();
 
-        // Alors la Valeur de la commission doit être de 10
+        // Alors la Valeur de la commission doit être de 1110
         $expected = 1110;
         $this->assertEquals($expected, $actual);
     }
